28f976fe8ae145c0cb269535484980819df4aeb5
toupk123 <751809460@qq.com>
2016-11-10 13:48:10 +0800

    Update 3.js

--- 3.js
+++ 3.js
@@ -1,5 +1,16 @@
-var s=2;
+(function(){
+ var request=new XMLHttpRequest(),
+     url="https://toupk123.github.io/dongman.githtb.io/1.json"
+  request.open("get",url);
+      request.onload=function(){
+       if(request.status==200){
+         sa(request.responseText);
+       }
+      }
+     request.send(null);
 function sa(s){
-alert(s);
+  var cc=JOSN.parse(s);
+  document.getElementById("s").innerHTML=cc;
+  alert(":s");
 }
-sa(s);
+ })();
