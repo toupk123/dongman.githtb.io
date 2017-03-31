7a9b742b17461e7336a968e0f6178b3efc046085
toupk123 <751809460@qq.com>
2017-03-31 19:06:48 +0800

    Update osSlider.js

--- name/osSlider.js
+++ name/osSlider.js
@@ -12,7 +12,7 @@ function osSlider(objs) {
     that.moveFlag = true;//添加是否可以进行下一个轮播状态
      that.width =objs.width;//得到容器的宽度
     that.height =objs.height;//得到容器的高度
-    console.log("sssssssss",that.width);
+    console.log("sssssssss",that.height);
     that.isPause = false;//是否暂停状态
     that.speedNum = 0;//自动轮播的计数
     if (!that.objs.speed) {//添加默认时间
